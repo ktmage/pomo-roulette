948b9870967cabb2a9bf47a2c2d901e3ac8d77b0
ktmage <yasashisainternet@gmail.com>
2024-05-05 21:51:21 +0900

    [add] : Settingsコンポーネント追加
    モーダルの設定画面を追加しました。

---
+++ src/components/modules/Settings/index.tsx
@@ -0,0 +1,10 @@
+export default function Settings() {
+	return (
+		<>
+			<h1>Settings</h1>
+			<hr />
+			<br />
+			<p>Settings will be added soon.</p>
+		</>
+	);
+}
--- src/components/modules/index.ts
+++ src/components/modules/index.ts
@@ -1,1 +1,2 @@
 export { default as Instructions } from './Instructions';
+export { default as Settings } from './Settings';
