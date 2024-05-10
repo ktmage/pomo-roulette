24593116b7798766667567dddd7ec41393951bd0
ktmage <yasashisainternet@gmail.com>
2024-05-11 01:16:38 +0900

    [update] : tailwind周りのアレコレ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
 	theme: {
 		extend: {},
 	},
