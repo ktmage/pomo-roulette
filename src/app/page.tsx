7dc589e91384e8c3f2ad77338db7ab183f1f167c
ktmage <yasashisainternet@gmail.com>
2024-05-12 23:14:35 +0900

    [update] : ごにょごにょ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-'use client';
-
 import { SettingsProvider } from '@/providers';
 import App from '@/components/pages/App';
 
 export default function RootPage() {
 	return (
-		<div>
-			<SettingsProvider>
-				<App />
-			</SettingsProvider>
-		</div>
+		<SettingsProvider>
+			<App />
+		</SettingsProvider>
 	);
 }
