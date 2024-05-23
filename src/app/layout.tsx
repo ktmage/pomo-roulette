411b1a62b8b80ad762df6793a49b23db90792391
ktmage <yasashisainternet@gmail.com>
2024-05-24 03:01:19 +0900

    [delete] : Backgroundを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Metadata } from 'next';
 import './globals.css';
 import { Navbar } from '@/components/modules';
 import AppProvider from '@/providers/AppProvider/AppProvider';
-import { Background } from '@/components/modules';
 
 export const metadata: Metadata = {
 	title: 'Pomodoro Timer - Pomomate',
@@ -13,14 +12,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang='en'>
 			<body>
-				<AppProvider>
-					<div id='root'>
-						<Background>
-							<Navbar title='Pomomate' />
-							{children}
-						</Background>
-					</div>
-				</AppProvider>
+				<div
+					id='root'
+					className='flex flex-col min-h-screen min-w-screen  transition-all duration-500 bg-base-100'
+				>
+					<AppProvider>
+						<Navbar />
+						{children}
+					</AppProvider>
+				</div>
 			</body>
 		</html>
 	);
