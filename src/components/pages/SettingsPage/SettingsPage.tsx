e2c2f392cf1de70441bb3defbc3f46f1253442ae
ktmage <yasashisainternet@gmail.com>
2024-05-25 23:29:58 +0900

    [update] : かなり冗長的だけどとりあえずレイアウトつくった。
    設定項目が増えてきたタイミングでちゃんとした設定画面を作りたい

--- src/components/pages/SettingsPage/SettingsPage.tsx
+++ src/components/pages/SettingsPage/SettingsPage.tsx
@@ -9,45 +9,43 @@ export default function SettingsPage() {
 
 	return (
 		// ここが強引なので、後で修正する
-		<div className='px-96 pt-12 flex flex-col gap-4 grow'>
+		<div className='flex flex-col gap-4 pt-16 items-center'>
 			<h2>Settings</h2>
-			<div className='flex flex-col gap-4'>
-				<label className='label cursor-pointer'>
-					<span className='label-text'>Task Mode</span>
-					<input
-						type='checkbox'
-						className='toggle'
-						checked={isTaskMode}
-						onChange={(e) => {
-							setIsTaskMode(e.target.checked);
-						}}
-					/>
-				</label>
+			<label className='label cursor-pointer w-64'>
+				<span className='label-text'>Task Mode</span>
+				<input
+					type='checkbox'
+					className='toggle'
+					checked={isTaskMode}
+					onChange={(e) => {
+						setIsTaskMode(e.target.checked);
+					}}
+				/>
+			</label>
 
-				<label className='label cursor-pointer'>
-					<span className='label-text'>Noise Mode</span>
-					<input
-						type='checkbox'
-						className='toggle'
-						checked={isNoiseMode}
-						onChange={(e) => {
-							setIsNoiseMode(e.target.checked);
-						}}
-					/>
-				</label>
+			<label className='label cursor-pointer w-64'>
+				<span className='label-text'>Noise Mode</span>
+				<input
+					type='checkbox'
+					className='toggle'
+					checked={isNoiseMode}
+					onChange={(e) => {
+						setIsNoiseMode(e.target.checked);
+					}}
+				/>
+			</label>
 
-				<label className='label cursor-pointer'>
-					<span className='label-text'>Long Break</span>
-					<input
-						type='checkbox'
-						className='toggle'
-						checked={isLongBreak}
-						onChange={(e) => {
-							setIsLongBreak(e.target.checked);
-						}}
-					/>
-				</label>
-			</div>
+			<label className='label cursor-pointer w-64'>
+				<span className='label-text'>Long Break</span>
+				<input
+					type='checkbox'
+					className='toggle'
+					checked={isLongBreak}
+					onChange={(e) => {
+						setIsLongBreak(e.target.checked);
+					}}
+				/>
+			</label>
 		</div>
 	);
 }
