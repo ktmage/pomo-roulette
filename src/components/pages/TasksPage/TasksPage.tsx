ed9adc43d2456071b8bf4670beaa6749c637941b
ktmage <yasashisainternet@gmail.com>
2024-05-26 16:17:17 +0900

    [add] : TasksPage.tsxを追加

--- src/components/pages/TasksPage/TasksPage.tsx
+++ src/components/pages/TasksPage/TasksPage.tsx
@@ -1,3 +1,61 @@
+'use client';
+
+import { TasksContext } from '@/providers';
+import { useContext } from 'react';
+
 export default function TasksPage() {
-	return <div>Tasks Page</div>;
+	const { tasks, newTask, setNewTask, addTask, deleteTask, updatePriority } =
+		useContext(TasksContext);
+
+	return (
+		<div className='flex flex-col gap-4 py-16 items-center'>
+			<h2 className='mb-8'>Tasks</h2>
+
+			<div className='flex flex-col gap-4'>
+				{tasks.map((task) => (
+					<div
+						key={task.id}
+						className='flex gap-4 items-center w-80 justify-between'
+					>
+						<span>{task.name}</span>
+						<div
+							className='flex gap-2
+                        items-center
+                        justify-between
+                    '
+						>
+							<input
+								type='number'
+								className='input input-bordered w-16'
+								value={task.priority}
+								onChange={(e) => updatePriority(task.id, parseInt(e.target.value))}
+							/>
+							<button
+								className='btn btn-sm'
+								onClick={() => deleteTask(task.id)}
+							>
+								Delete
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+
+			<div className='flex gap-4 mt-8'>
+				<input
+					type='text'
+					className='input input-bordered'
+					placeholder='New Task'
+					value={newTask}
+					onChange={(e) => setNewTask(e.target.value)}
+				/>
+				<button
+					className='btn'
+					onClick={addTask}
+				>
+					Add
+				</button>
+			</div>
+		</div>
+	);
 }
