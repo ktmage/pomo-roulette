94e139aab071584792854d801492538c2ae16314
ktmage <yasashisainternet@gmail.com>
2024-05-30 23:36:55 +0900

    [update] : loading.tsxの修正
    デザインを変更しました。

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,12 +2,11 @@ export default function Loading() {
 	return (
 		<div
 			className='
-            flex
-            items-center
-            justify-center
-            h-screen
-            w-screen
-        '
+        flex
+        items-center
+        justify-center
+        h-full
+    '
 		>
 			<span className='loading loading-spinner loading-lg'></span>
 		</div>
